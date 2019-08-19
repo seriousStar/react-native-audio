24e3018119e1c18d50494305eaf2a83df465a953
Rati Kirvalidze <rati.kirvalidze10@gmail.com>
2019-08-19 16:47:25 +0400

    set iOS audio to continue playing when playing music

--- ios/AudioRecorderManager.m
+++ ios/AudioRecorderManager.m
@@ -230,7 +230,8 @@ RCT_EXPORT_METHOD(prepareRecordingAtPath:(NSString *)path sampleRate:(float)samp
       [_recordSession setCategory:AVAudioSessionCategoryRecord error:nil];
       [_recordSession setMode:AVAudioSessionModeMeasurement error:nil];
   }else{
-      [_recordSession setCategory:AVAudioSessionCategoryMultiRoute error:nil];
+//      [_recordSession setCategory:AVAudioSessionCategoryMultiRoute error:nil];
+      [_recordSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionMixWithOthers error:nil];
   }
 
   _audioRecorder = [[AVAudioRecorder alloc]
